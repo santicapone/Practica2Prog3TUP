a9ab3e7ca01318c9cf6a979cfcac0450cb50659e
efalabrini <edfsoft@gmail.com>
2024-03-31 20:18:58 -0300

    Ej1 solved

---
+++ Web/Controllers/Ej1Controller.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Web.Ej1;
+
+namespace Web.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class Ej1Controller : ControllerBase
+    {
+        [HttpGet()]
+        public List<string> Get([FromQuery] string persona1, [FromQuery] string persona2, [FromQuery] string persona3 )
+        {
+            List<Persona> lista = [];
+            lista.Add(new Persona(persona1));
+            lista.Add(new Persona(persona2));
+            lista.Add(new Persona(persona3));
+
+            List<string> result = [];
+
+            foreach (Persona persona in lista)
+            {
+                result.Add(persona.GetSaludo());
+            }
+
+            return result;
+        }
+    }
+}
---
+++ Web/Ej1/Persona.cs
@@ -0,0 +1,17 @@
+﻿namespace Web.Ej1
+{
+    public class Persona
+    {
+        public string Nombre { get; set; }
+
+        public Persona(string nombre)
+        {
+            Nombre = nombre;
+        }
+
+        public string GetSaludo()
+        {
+            return $"Hola mi nombre es {Nombre}";
+        }
+    }
+}
